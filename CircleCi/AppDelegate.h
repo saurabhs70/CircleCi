7821d749a5a86e880f2eb5b0de52ec456acae620
Jasmeet Singh <dinnysingh@gmail.com>
2017-06-09 15:58:39 +0530

    ee

---
+++ CircleCi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleCi
+//
+//  Created by umenit on 6/9/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleCi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CircleCi
+//
+//  Created by umenit on 6/9/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
